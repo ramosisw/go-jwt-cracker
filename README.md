509bf52abd3d9974b378d1c5810b6905cee92af8
ramosisw <ramos.isw@gmail.com>
2020-02-12 16:58:29 -0600

    optimize

--- README.md
+++ README.md
@@ -5,39 +5,42 @@ It heavly relies on this JWT library: `github.com/dgrijalva/jwt-go`
 This tool supports both wordlist and bruteforce based attacks.
 
 ## Installation
-Build packages will be available soon for the majority of the platforms, but in the meantime you can build it yourself:
-
 ```
-git clone https://github.com/riccardoancarani/go-jwt-cracker.git
-cd go-jwt-cracker
-export GOPATH=$(pwd)
-go get github.com/dgrijalva/jwt-go
-cd src/app
-go install
+go install github.com/ramosisw/go-jwt-cracker
 ```
 You'll find the binary under the `bin` directory.
 
 ## Usage
 This is a CLI tool, this means that it is meant to be used from your shell/terminal.
 
-### Main options
-- `--token`: The token you want to crack
-- `--brute`: Start the brute force attack
-- `--wordlist <file>`: The file for wordlist attack
-- `--charset <charset>`: Specify the charset to use in the bruteforce attack
-- `--max`: The upper limit of the string's lenght for the brute force attack
+```txt
+go-jwt-cracker is a simple tool used to bruteforce HMAC secret keys in JWT tokens
+
+Usage:
+
+	go-jwt-cracker [arguments]
+
+The arguments are:
+	--token <token>       The token you want to crack
+	--wordlist <file>     The file for wordlist attack
+	--charset <charset>   Specify the charset to use in the bruteforce attack
+	--max <number>        The lower limit of the string's length for the brute force attack
+	--min <number>        The upper limit of the string's length for the brute force attack
+	--brute               Start the brute force attack
+
+```
 
 ## Examples
-In this examples I'll use this token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o`
+In this examples I'll use this token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.uJ0gEbC69N61cc2wHHl5vnC6uZMyLYUSwwQjwbYguqE`
 
-that has been signed with the secret = `secret`.
+that has been signed with the secret = `qwertyui`.
 
 ### Example 1 - Brute force
 ```
-./app --token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o --brute
+go-jwt-cracker --token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.uJ0gEbC69N61cc2wHHl5vnC6uZMyLYUSwwQjwbYguqE --brute
 ```
 
 ### Example 2 - Wordlist mode
 ```
-./app --token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o --wordlist dict.txt[+] Valid secret found: secret
+go-jwt-cracker --token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.uJ0gEbC69N61cc2wHHl5vnC6uZMyLYUSwwQjwbYguqE --wordlist dict.txt[+] Valid secret found: secret
 ```
--- main.go
+++ main.go
@@ -9,15 +9,40 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
+func printHelp() {
+	fmt.Println(`go-jwt-cracker is a simple tool used to bruteforce HMAC secret keys in JWT tokens
+
+Usage:
+
+	go-jwt-cracker [arguments]
+
+The arguments are:
+	--token <token>       The token you want to crack
+	--wordlist <file>     The file for wordlist attack
+	--charset <charset>   Specify the charset to use in the bruteforce attack
+	--max <number>        The lower limit of the string's length for the brute force attack
+	--min <number>        The upper limit of the string's length for the brute force attack
+	--brute               Start the brute force attack
+ `)
+}
+
 func main() {
 
 	var tokenString = flag.String("token", "", "The JWT token you want to crack")
 	var wordlist = flag.String("wordlist", "", "The wordlist you want to use")
 	var brute = flag.Bool("brute", false, "Use bruteforce mode")
 	var charset = flag.String("charset", "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM", "The charset to use during bruteforce")
+	var minChar = flag.Int("min", 7, "Min chars in token")
 	var maxChar = flag.Int("max", 12, "Max chars in token")
 
 	flag.Parse()
+
+	if string(*tokenString) == "" {
+		printHelp()
+		return
+	}
+
+	fmt.Println("Cracking...")
 
 	if *wordlist != "" && *brute == false {
 		wordlistFile, _ := os.Open(*wordlist)
@@ -36,10 +61,13 @@ func main() {
 	}
 
 	if *brute == true {
-		for combination := range GenerateCombinations(*charset, *maxChar) {
-			if validateToken(*tokenString, combination) {
-				fmt.Println("[+] Valid secret found: " + combination)
-				return
+		for lenChar := *minChar; lenChar <= *maxChar; {
+			fmt.Printf("Trying with lentgth: %v \n", lenChar)
+			for combination := range generateCombinations(*charset, lenChar) {
+				if validateToken(*tokenString, combination) {
+					fmt.Println("[+] Valid secret found: " + combination)
+					return
+				}
 			}
 		}
 	}
@@ -48,30 +76,28 @@ func main() {
 
 // I found this function in a stackoverflow answer that I'm not finding anymore
 // when I'll find it I'll give appropriate credits to the author
-func GenerateCombinations(alphabet string, length int) <-chan string {
+func generateCombinations(alphabet string, length int) <-chan string {
 	c := make(chan string)
 	go func(c chan string) {
-		defer close(c) 
-		
-		AddLetter(c, "", alphabet, length) 
+		defer close(c)
+
+		addLetter(c, "", alphabet, length)
 	}(c)
-	return c 
+	return c
 }
 
-func AddLetter(c chan string, combo string, alphabet string, length int) {
+func addLetter(c chan string, combo string, alphabet string, length int) {
 	if length <= 0 {
 		return
 	}
-	
+
 	var newCombo string
 	for _, ch := range alphabet {
 		newCombo = combo + string(ch)
 		c <- newCombo
-		AddLetter(c, newCombo, alphabet, length-1)
-	}	
+		addLetter(c, newCombo, alphabet, length-1)
+	}
 }
-
-
 
 func validateToken(tokenString string, secret string) bool {
 	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
@@ -82,10 +108,5 @@ func validateToken(tokenString string, secret string) bool {
 		hmacSampleSecret := []byte(secret)
 		return hmacSampleSecret, nil
 	})
-
-	if token.Valid {
-		return true
-	} else {
-		return false
-	}
+	return token.Valid
 }
